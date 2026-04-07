d777ff81d7672419ce8f279ac67cfd9095f6b567
Giovanna <giovanna.soliveira2021@gmail.com>
2026-04-07 10:30:58 -0300

    adicionando a lógica inicial do jogo par ou impar

---
+++ jogo.py
@@ -0,0 +1,20 @@
+# criando variaveis para o jogo
+jogador1 = int(input("informe um número: "))
+jogador2 = int(input('informe um número: '))
+
+# operadores matemáticos
+#somar + ex: 1+1 = 2
+# incrementar += ex: idade += 1 
+# subtração - ex: 3-2 = 1
+#decremento -= ex:idade -= 1 
+#multiplicacao *
+#divisao / - ex:3/2 = 1.5
+# módulo % - ex:3%2 = 1 - traz o resto 
+
+numero = jogador1 + jogador2
+
+# lógica - if e else
+if numero % 2 != 0:
+    print("impar ganhou")
+else:
+    print('par ganhou')
